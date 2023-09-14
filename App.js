200c2bebdbcb653fe215698b6c435cd0d61dc5d6
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-14 18:48:16 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>React Router</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
