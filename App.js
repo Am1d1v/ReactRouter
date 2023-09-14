9f566585dc75029f95927eadc638e9fd84cc814d
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-14 19:32:23 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -11,6 +11,9 @@ function App() {
       <div className="App">
         <Routes>
           <Route path='/' element={<h1>Home</h1>} />   
+          <Route path='about' element={<h1>About</h1>} />   
+          <Route path='contacts' element={<h1>Contacts</h1>} />   
+          <Route path='*' element={<h1>Not Found</h1>} />   
         </Routes>
       </div>
     </BrowserRouter>
